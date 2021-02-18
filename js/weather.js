32e4d6b9c74c67e307a183b90d39c289f1b5302f
Hydrochloric-Acid168 <79241552+Hydrochloric-Acid168@users.noreply.github.com>
2021-02-18 11:28:33 -0800

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -13,7 +13,7 @@ var tempUnit = 'F';
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
-const key = 'aa5b0a76dfbf87441928fb3cc32d3d72';
+const key = 'ed984e7ed3a6554b4521e86349f99c86';
 
 // Set Position function
 setPosition();
@@ -21,8 +21,8 @@ setPosition();
 function setPosition(position) {
   // Here you can change your position
   // You can use https://www.latlong.net/ to get it! (I use San Francisco as an example)
-  let latitude = 37.774929;
-  let longitude = -122.419418;
+  let latitude = 37.368832;
+  let longitude = -122.036346;
 
   getWeather(latitude, longitude);
 }
